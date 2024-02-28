81311165c7425bf0ffc6ac9365eef967e4db1b40
2024-CAT <habib.sy@cat.edu.sn>
2024-02-28 23:05:44 +0000

    wednesday

--- component/inc/ectf_params.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ECTF_PARAMS__
-#define __ECTF_PARAMS__
-#define COMPONENT_ID 286331154
-#define COMPONENT_BOOT_MSG "boot comp2"
-#define ATTESTATION_LOC "Dakar"
-#define ATTESTATION_DATE "02/02/24"
-#define ATTESTATION_CUSTOMER "fatou"
-#endif
