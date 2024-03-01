d4b74e7a4ef7fd06508dcac78f0ff59493eca8df
2024-CAT <habib.sy@cat.edu.sn>
2024-03-01 13:33:01 +0000

    initial

--- application_processor/src/board_link.c
+++ application_processor/src/board_link.c
@@ -49,8 +49,8 @@ i2c_addr_t component_id_to_i2c_addr(uint32_t component_id) {
  * Function sends an arbitrary packet over i2c to a specified component
 */
 int send_packet(i2c_addr_t address, uint8_t len, uint8_t* packet) {
-    if (len <= MAX_I2C_MESSAGE_LEN)
-    {
+    // if (len <= MAX_I2C_MESSAGE_LEN)
+    // {
         int result;
     result = i2c_simple_write_receive_len(address, len);
     if (result < SUCCESS_RETURN) {
@@ -66,7 +66,7 @@ int send_packet(i2c_addr_t address, uint8_t len, uint8_t* packet) {
     }
 
     return SUCCESS_RETURN;
-    }   
+    // }   
 }
 
 /**
@@ -92,8 +92,8 @@ int poll_and_receive_packet(i2c_addr_t address, uint8_t* packet) {
     }
 
     int len = i2c_simple_read_transmit_len(address);
-    if (len <= MAX_I2C_MESSAGE_LEN)
-        {
+    // if (len <= MAX_I2C_MESSAGE_LEN)
+    //     {
             if (len < SUCCESS_RETURN) {
             return ERROR_RETURN;
         }
@@ -105,6 +105,6 @@ int poll_and_receive_packet(i2c_addr_t address, uint8_t* packet) {
         if (result < SUCCESS_RETURN) {
             return ERROR_RETURN;
         }
-    } 
+    // } 
     return len;
 }
